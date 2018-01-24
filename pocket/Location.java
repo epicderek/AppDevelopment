5f896dd1c25f8a66a98573a3367ce724df52d00c
Your Name <you@example.com>
2018-01-25 05:09:54 +0800

    Brief Modification.

--- pocket/Location.java
+++ pocket/Location.java
@@ -1,6 +1,11 @@
 package pocket;
 import static pocket.Sub.*;
 
+/**
+ * A representation of either a physical location or an abstract one in the software space. 
+ * @author Maggie
+ *
+ */
 public interface Location 
 {
 	void appendEvents(Event... eve);
